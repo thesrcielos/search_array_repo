726dd6816b3683247eeb8b4c8f3b3853a09cf199
thesrcielos <diegomadi75@gmail.com>
2025-03-20 20:12:47 -0500

    Adding visualization

--- app.py
+++ app.py
@@ -1,15 +1,92 @@
 import sys
 from arraysearch import algorithms
 from arraysearch import execution_time_gathering
+import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
+def graphicsAlgorithms():
     minimum_size = 100000
-    maximum_size = 500000
-    step = 25000
-    samples_by_size = 7
+    maximum_size = 200000
+    step = 10000
+    samples_by_size = 5
     
     table = execution_time_gathering.take_execution_time(minimum_size, maximum_size, step, samples_by_size)
 
-    print("Size | Linear Search| Binary Search | Ternany Search")
+    print("Size | Linear Search | Binary Search | Ternary Search")
     for row in table:
         print(row)
+
+
+    # La primera columna será el eje X
+    x = [row[0] for row in table]
+    # Las siguientes columnas serán los valores de Y
+    y1 = [row[1][0] for row in table]
+    y2 = [row[2][0] for row in table]
+    y3 = [row[3][0] for row in table]
+
+    # Crear el gráfico
+    plt.plot(x, y1, label="Linear", color='blue')
+    plt.plot(x, y2, label="Binary", color='green')
+    plt.plot(x, y3, label="Ternary", color='red')
+
+    plt.title('Comparation Search in Array algorithms')
+    plt.xlabel('Size')
+    plt.ylabel('Time')
+    plt.legend()
+    plt.show()
+
+    y1 = [row[1][1] for row in table]
+    y2 = [row[2][1] for row in table]
+    y3 = [row[3][1] for row in table]
+
+    plt.plot(x, y1, label="Linear", color='blue')
+    plt.plot(x, y2, label="Binary", color='green')
+    plt.plot(x, y3, label="Ternary", color='red')
+
+    plt.title('Comparation Search in Array algorithms Memory Usage')
+    plt.xlabel('Size')
+    plt.ylabel('Memory')
+    plt.legend()
+    plt.show()
+
+def graphicsBinaryTernary():
+    minimum_size = 200000
+    maximum_size = 1000000
+    step = 100000
+    samples_by_size = 5
+    table = execution_time_gathering.take_execution_time(minimum_size, maximum_size, step, samples_by_size, True)
+    print("Size | Binary Search | Ternary Search")
+    for row in table:
+        print(row)
+
+    # La primera columna será el eje X
+    x = [row[0] for row in table]
+    # Las siguientes columnas serán los valores de Y
+    y1 = [row[1][0] for row in table]
+    y2 = [row[2][0] for row in table]
+
+    # Crear el gráfico
+    plt.plot(x, y1, label="Binary", color='blue')
+    plt.plot(x, y2, label="Ternary", color='green')
+
+    plt.title('Comparation Search in Array algorithms')
+    plt.xlabel('Size')
+    plt.ylabel('Time')
+    plt.legend()
+    plt.show()
+
+    y1 = [row[1][1] for row in table]
+    y2 = [row[2][1] for row in table]
+
+    plt.plot(x, y1, label="Binary", color='blue')
+    plt.plot(x, y2, label="Ternary", color='green')
+
+    plt.title('Comparation Sorting algorithms Memory Usage')
+    plt.xlabel('Size')
+    plt.ylabel('Memory')
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    graphicsAlgorithms()
+    graphicsBinaryTernary()
--- arraysearch/execution_time_gathering.py
+++ arraysearch/execution_time_gathering.py
@@ -1,18 +1,18 @@
 import time
-
+import tracemalloc
 import random
 from arraysearch import algorithms
 from arraysearch import constants
 from arraysearch import data_generator
 
 
-def take_execution_time(minimum_size, maximum_size, step, samples_by_size):
+def take_execution_time(minimum_size, maximum_size, step, samples_by_size, two=False):
     return_table = []
 
     for size in range(minimum_size, maximum_size + 1, step):
         print("Processing size: " + str(size))
         table_row = [size]
-        times = take_times(size, samples_by_size)
+        times = take_times(size, samples_by_size, two)
         return_table.append(table_row + times)
 
     return return_table
@@ -23,11 +23,16 @@ def take_execution_time(minimum_size, maximum_size, step, samples_by_size):
 """
 
 
-def take_times(size, samples_by_size):
+def take_times(size, samples_by_size, two):
     samples = []
     for _ in range(samples_by_size):
         samples.append(data_generator.get_random_list(size))
 
+    if two:
+        return [
+            take_time_for_algorithm(samples, algorithms.binary_search),
+            take_time_for_algorithm(samples, algorithms.ternary_search),
+        ]
     return [
         take_time_for_algorithm(samples, algorithms.linear_search),
         take_time_for_algorithm(samples, algorithms.binary_search),
@@ -42,12 +47,19 @@ def take_times(size, samples_by_size):
 
 def take_time_for_algorithm(samples_array, searching_approach):
     times = []
+    memory_usage = []
 
     for sample in samples_array:
+        tracemalloc.start()
         elem = sample[random.randint(0, len(sample))]
         start_time = time.time()
         searching_approach(sample.copy(), elem)
-        times.append(int(constants.TIME_MULTIPLIER * (time.time() - start_time)))
+        end_time = time.time()
+        _, peak = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+        times.append(int(constants.TIME_MULTIPLIER * (end_time - start_time)))
+        memory_usage.append(peak)
 
     times.sort()
-    return times[len(times) // 2]
+    memory_usage.sort()
+    return times[len(times) // 2], memory_usage[len(memory_usage) // 2]
